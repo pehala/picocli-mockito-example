be22a8b94a34dc6a7d6014fe4c0cb87663b7d50a
Petr Hála <halapetr@selfnet.cz>
2020-11-03 08:31:39 +0100

    Add Mock and MockWithoutAnnotations test cases

--- src/test/java/org/example/CheckSumTest.java
+++ src/test/java/org/example/CheckSumTest.java
@@ -3,15 +3,27 @@ package org.example;
 import org.junit.jupiter.api.Test;
 import picocli.CommandLine;
 
-import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.*;
 
 class CheckSumTest {
 
     @Test
     void testSpy() {
-        CheckSum launcher = spy(new CheckSum());
+        CheckSum launcher = spy(CheckSum.class);
 
         new CommandLine(launcher);
+    }
+
+    @Test
+    void testMock() {
+        CheckSum checkSum = mock(CheckSum.class);
+        new CommandLine(checkSum);
+    }
+
+    @Test
+    void testMockWithoutAnnotations() {
+        CheckSum checkSum = mock(CheckSum.class, withSettings().withoutAnnotations());
+        new CommandLine(checkSum);
     }
 
     @Test
